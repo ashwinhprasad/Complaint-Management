73ce1cadfe7e21fa49075e9acccfc0d68a8fd12a
ashwinhprasad <ashwinprasad202@gmail.com>
2021-11-06 22:56:52 +0530

    added image upload to models

--- backend/complaints/models.py
+++ backend/complaints/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class ComplaintModel(models.Model):
     title = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='uploads/')
 
     def __str__(self):
         return f"{self.id} - {self.title}"
