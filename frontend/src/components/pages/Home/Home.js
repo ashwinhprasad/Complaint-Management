b09c94774e468646164fc66f2828a540de55ca2c
ashwinhprasad <ashwinprasad202@gmail.com>
2021-11-12 20:51:54 +0530

    Image Upload

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,13 +1,34 @@
 import { useHistory } from "react-router-dom"
-import { useEffect, useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 import { tokenContext } from "../../../App"
 import {Button, Row, Col } from "react-bootstrap"
+import axios from "axios"
 
 const Home = () => {
 
     const history = useHistory()
     const [token,setToken] = useContext(tokenContext)
+    const [complaint,setComplaint] = useState({
+        'title':'',
+        'photo':''
+    })
 
+    const onComplaintPost = async (e) => {
+        const data = new FormData()
+        data.append('title',complaint.title)
+        data.append('photo',complaint.photo)
+        const res = await axios.post('http://localhost:8000/api/complaint/',data,{
+            'headers':{
+                'Authorization': `Token ${token}` 
+            } 
+        })
+        console.log(res)
+        setComplaint({
+            'title':'',
+            'photo':''
+        })
+    }
+    
     useEffect(() => {
     if(localStorage.getItem("token")){
         setToken(localStorage.getItem("token"))
@@ -21,11 +42,16 @@ const Home = () => {
         <div>
             <form className="complaint-form">
                 <h2>File Complaint</h2>
-                <input type="text" name="title" placeholder="Complaint Title"/>
-                <input type="file" name="proof" />
+                <input type="text" name="title" placeholder="Complaint Title" 
+                value={complaint.title}
+                onChange={(e) => setComplaint({...complaint,'title':e.target.value})} 
+                />
+                <input type="file"  name="proof"
+                onChange={(e) => setComplaint({...complaint,'photo':e.target.files[0]})}  
+                />
                 <Row>
                     <Col>
-                        <Button variant="success" >Post</Button>
+                        <Button variant="success" onClick={onComplaintPost} >Post</Button>
                     </Col>
                     <Col>
                         <Button variant='danger'>Logout</Button>
